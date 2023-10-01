4f103bbc47722e8fb16ba827e9dd392d7845cce7
hjingkai <hjingkai1125@gmail.com>
2023-10-01 09:03:42 +0800

    pu;;

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Item from '../Item'
@@ -26,32 +25,3 @@ export default class List extends Component {
     )
   }
 }
-=======
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import Item from '../Item'
-import './index.css'
-
-export default class List extends Component {
-
-  //對接收的props進行:類型、必要性的限制
-  static propTypes = {
-    todos:PropTypes.array.isRequired,
-    updateTodo:PropTypes.func.isRequired,
-    deleteTodo:PropTypes.func.isRequired,
-  }
-
-  render() {
-    const {todos,updateTodo,deleteTodo} = this.props
-    return (
-        <ul className="todo-main">
-          {
-            todos.map( todo =>{
-              return <Item key={todo.id} {...todo} updateTodo={updateTodo} deleteTodo={deleteTodo}/>
-            })
-          }
-        </ul>
-    )
-  }
-}
->>>>>>> 880ec5654ff783a2c900d41823380450b40ff914
